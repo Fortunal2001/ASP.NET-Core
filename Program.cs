e6825e76a135baff818673ee51343e6b50564f2b
amalthambicapgemini <87962669+amalthambicapgemini@users.noreply.github.com>
2024-04-15 16:43:31 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-// modified by amal thambi
+// modified by amal thambi 1
 namespace pipelines_dotnet_core
 {
     public class Program
